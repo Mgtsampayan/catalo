4e90912ffb4cc796cba6b6714a91f4547ad6fab8
Mgtsampayan <90001725+Mgtsampayan@users.noreply.github.com>
2023-03-03 16:26:51 +0800

    Add files via upload

---
+++ Django-Project-Catalo/mysite/myapp_catalo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyappApuliConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myapp_apuli'
---
+++ Django-Project-Catalo/mysite/myapp_catalo/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render 
+def index(request): # < here 
+    return render(request, 'myapp/index.html') 
